042c9d7ba34bc35bcac3f0ab86414ab94bd2d9a4
Alexey Pertsukh <85017966+AlexeyPertsukh@users.noreply.github.com>
2021-10-14 11:38:48 +0300

    remade getValueOrEmptyStringIfNull

--- app/src/main/java/com/example/jsonplaceholder/Util.java
+++ app/src/main/java/com/example/jsonplaceholder/Util.java
@@ -1,11 +1,13 @@
 package com.example.jsonplaceholder;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class Util {
     private Util() {
     }
 
+    //юзеры с частичным заполнением инфо для тестирования вывода на gui
     public static ArrayList<User> getTestUsers() {
         ArrayList<User> users = new ArrayList<>();
         //User(long id, String name, String username, String email, String phone, String website, Address address, Company company)
@@ -13,6 +15,7 @@ public class Util {
                 "mytest1_incomplete",null,
                 "+38(068)908-29-21","plc-blog.com.ua",
                 null, null));
+
         users.add(new User(0, "Daniil Laureanti",
                 "mytest2_empty",null,
                 null,null,
@@ -20,11 +23,12 @@ public class Util {
         return users;
     }
 
+    private static final String EMPTY_STRING = "";
+
     public static String getValueOrEmptyStringIfNull(String value) {
-        if(value == null) {
-            return "";
-        } else {
-            return value;
-        }
+        return (value == null) ? EMPTY_STRING : value;
+
+//        Optional<String> optional = Optional.ofNullable(value);   //не работает в старых api
+//        return optional.orElse("");
     }
 }
